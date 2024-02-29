8fa15ac243f66e545cea65c6424c327f970d9874
jhw0900 <jhw0900@gmail.com>
2024-02-29 18:10:37 +0900

    로그인&로그아웃 페이지 라우팅, 세션 유지 기능 구현, 메인페이지 헤더 디자인

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from busAPI import *;
 from auth import login 
-from flask import Flask, render_template, request, session, redirect, url_for;
+from flask import Flask, render_template, request, session, redirect, url_for, g;
 app = Flask(__name__)
 app.secret_key = "super secret";
 app.config['JSON_AS_ASCII'] = False;
@@ -15,6 +15,8 @@ def main():
 # Login Page
 @app.route("/login", methods=["GET"])
 def login_page():
+    if g.user:
+        return redirect(url_for("main"));
     return render_template("login.html");
 
 # Login Process
@@ -29,6 +31,18 @@ def login_proc():
         session["cookies"] = res_cookies;
         return redirect(url_for("main"));
     else: return redirect(url_for("login_page"));
+
+# Logout Process
+@app.route("/logout", methods=["GET"])
+def logout():
+    session.clear();
+    return redirect(url_for("main"));
+
+@app.before_request
+def load_logged_in_user():
+    if "cookies" in session:
+        g.user = True;
+    else: g.user = None;
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5001, debug=True);
